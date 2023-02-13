441daeea01d6a7d853306b2300458d3612feac37
Zainab Nisa <115335614+ZainabNisa786@users.noreply.github.com>
2023-02-13 20:11:41 +0500

    Update index.js

--- index.js
+++ index.js
@@ -1,17 +1,4 @@
 
-// let countEl = document.getElementById("count-num")
-
-// let count=0;
-// function increment(){
-//     count=count+1
-//     countEl.innerText= count+1
-// }
-
-// function reset(){
-//     count=count-1
-//     countEl.innerText=count-1
-// }
-
 let countEl=document.getElementById("count-el")
 let saveEl=document.getElementById("save-el")
 let count=0
